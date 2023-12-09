da0ce58f81bc926738635a7f2c108c952a3f2cf3
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-12-09 18:54:30 +0200

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Yulian Gyuroff on 9.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
